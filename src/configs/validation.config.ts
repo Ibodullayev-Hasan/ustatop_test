553ac707ecaac1fef1fe547c82df32198c905883
Ibodullayev-Hasan <ibodullayevh040@gmail.com>
2025-08-20 16:43:01 +0500

    SetUpValidationConfig func created

---
+++ src/configs/validation.config.ts
@@ -0,0 +1,15 @@
+import { BadRequestException, INestApplication, ValidationPipe } from "@nestjs/common";
+
+export const SetUpValidationConfig = (app: INestApplication) => {
+	app.useGlobalPipes(new ValidationPipe({
+		whitelist: true,
+		forbidNonWhitelisted: true,
+		transform: true,
+		exceptionFactory: (errors) => {
+			const messages = errors.flatMap(error =>
+				Object.values(error.constraints || {})
+			);
+			return new BadRequestException(messages);
+		},
+	}))
+}
